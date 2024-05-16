b05a55921382fcfbd4f2cf7282a91aa9bf52e250
Emil De Guzman <em.deguzm@gmail.com>
2024-05-16 12:19:56 -0700

    add dom

--- section-15-intermediate-javascript/fibonacci.js
+++ section-15-intermediate-javascript/fibonacci.js
@@ -5,28 +5,23 @@ var thirdNumber = 0;
 
 function fibonacciGenerator(n) {
   if (n === 0) {
-    console.log(output);
     return (output = []);
   }
   if (n === 1) {
-    console.log(output);
     return (output = [0]);
   }
   if (n === 2) {
-    console.log(output);
     return (output = [0, 1]);
   }
   output = [0, 1];
   for (let i = 2; i < n; i++) {
     thirdNumber = output[i - 1] + output[i - 2];
     output.push(thirdNumber);
-    console.log(output);
   }
   return output;
 }
 
 // [0,1,1,2,3,5,8]
 
-fibonacciGenerator(10);
-fibonacciGenerator(1);
-fibonacciGenerator(2);
+fibonacciGenerator(500);
+console.log(output);
