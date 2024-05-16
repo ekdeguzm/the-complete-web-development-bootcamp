43925a6101f7f9bb79e3c27cda566de3b53b5591
Emil De Guzman <em.deguzm@gmail.com>
2024-05-16 10:42:39 -0700

    create fibonacci sequence code

--- section-15-intermediate-javascript/fibonacci.js
+++ section-15-intermediate-javascript/fibonacci.js
@@ -1,10 +1,32 @@
 var output = [];
+var firstNumber = 0;
+var secondNumber = 1;
+var thirdNumber = 0;
 
 function fibonacciGenerator(n) {
   if (n === 0) {
-    return (output = [0]);
+    console.log(output);
+    return (output = []);
   }
   if (n === 1) {
+    console.log(output);
+    return (output = [0]);
+  }
+  if (n === 2) {
+    console.log(output);
     return (output = [0, 1]);
   }
+  output = [0, 1];
+  for (let i = 2; i < n; i++) {
+    thirdNumber = output[i - 1] + output[i - 2];
+    output.push(thirdNumber);
+    console.log(output);
+  }
+  return output;
 }
+
+// [0,1,1,2,3,5,8]
+
+fibonacciGenerator(10);
+fibonacciGenerator(1);
+fibonacciGenerator(2);
