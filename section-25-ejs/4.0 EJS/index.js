2d18a1a462167b1f01a92b09a0ac6442e50c8836
Emil De Guzman <em.deguzm@gmail.com>
2024-05-31 11:08:11 -0700

    add specific date

--- section-25-ejs/4.0 EJS/index.js
+++ section-25-ejs/4.0 EJS/index.js
@@ -3,13 +3,25 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+const week = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
 app.get("/", (req, res) => {
   const today = new Date();
+  console.log(today);
   const day = today.getDay();
   console.log(day);
 
   let type = "a weekday";
   let adv = "it's time to work hard";
+  let specific = week[day];
 
   if (day === 0 || day === 6) {
     type = "weekend";
@@ -17,6 +29,7 @@ app.get("/", (req, res) => {
   }
 
   res.render("index.ejs", {
+    daySpecific: specific,
     dayType: type,
     advice: adv,
   });
