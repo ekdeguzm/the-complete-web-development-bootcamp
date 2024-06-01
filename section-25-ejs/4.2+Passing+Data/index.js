b0fc9894b4fde7e5e0a3191d7c59b58ce84411e2
Emil De Guzman <em.deguzm@gmail.com>
2024-06-01 12:23:10 -0700

    complete passing data to server

--- section-25-ejs/4.2+Passing+Data/index.js
+++ section-25-ejs/4.2+Passing+Data/index.js
@@ -6,9 +6,21 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const nameLength = firstName.length + lastName.length;
+
+  console.log(nameLength);
+
+  res.render("index.ejs", {
+    nameLength: nameLength,
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
